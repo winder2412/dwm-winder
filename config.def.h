a153325920fe37717b0da39d7482217c0b6060ec
winder2412 <winderdawshmaster@gmail.com>
2023-07-02 17:08:36 +0330

    added some stuff

--- config.def.h
+++ config.def.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *filecmd[]  = { "nautilus", NULL };
+static const char *filecmd[]  = { "dolphin", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 static const char *roficmd[]  = { "rofi", "-show","drun", NULL };
 
